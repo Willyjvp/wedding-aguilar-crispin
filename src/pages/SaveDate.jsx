7caafa750b28c9d2a65b0c5393c0368a2acb1619
Willyjvp <willy.vas.p@gmail.com>
2023-01-11 08:31:06 -0600

    adding save the date page

---
+++ src/pages/SaveDate.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './SaveDate.module.css'
+
+const SaveDate = () => {
+  return (
+    <div className={styles.container}>
+        <div className={styles.backgroundContainer}/>
+        <div className={styles.dateText}/>
+    </div>
+  )
+}
+
+export default SaveDate
